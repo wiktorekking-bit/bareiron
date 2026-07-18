26e334731cd904e517e75e783abc00e2924c5e69
wiktorekking-bit <wiktorekking@gmail.com>
2026-07-18 11:02:40 +0200

    Update globals.c

--- src/globals.c
+++ src/globals.c
@@ -37,7 +37,7 @@ uint32_t rng_seed = INITIAL_RNG_SEED;
 uint16_t world_time = 0;
 uint32_t server_ticks = 0;
 
-char motd[] = { "A bareiron server" };
+char motd[] = { "AmongusIsSus" };
 uint8_t motd_len = sizeof(motd) - 1;
 
 #ifdef SEND_BRAND
