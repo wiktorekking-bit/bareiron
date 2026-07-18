b665cf1a6f2dfd7534dbd4ffbdb3df45ff9bbc44
wiktorekking-bit <wiktorekking@gmail.com>
2026-07-18 22:06:31 +0200

    Rename placeVillageHouse to placeHouse and add new functions

--- include/structures.h
+++ include/structures.h
@@ -2,6 +2,9 @@
 #define H_STRUCTURES
 
 void placeTreeStructure (short x, uint8_t y, short z);
-void placeVillageHouse(short x, uint8_t y, short z);
+void placeHouse(short x, uint8_t y, short z);
+void placePath(short x, uint8_t y, short z, int length);
+void placeVillage(short x, uint8_t y, short z);
+
 
 #endif
